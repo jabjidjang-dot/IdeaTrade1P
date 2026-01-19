0a3cd29a06c75136a355b61b2020eacae7639d00
Nitchanun041 <6531501041@lamduan.mfu.ac.th>
2026-01-19 10:13:01 +0700

    React + MUI

---
+++ idt1/src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Button from "@mui/material/Button";
+
+function App() {
+    return (
+    <div style={{ padding: 40 }}>
+      <Button variant="contained">Hello MUI</Button>
+    </div>
+  );
+}
+
+export default App;
